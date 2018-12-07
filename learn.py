d8f18adbf7c33aa433f78bcadd926d3ed3c199e2
sylva <ate_sylva@yahoo.com>
2018-12-06 23:32:05 -0700

    added the py file

--- learn.py
+++ learn.py
@@ -3,6 +3,7 @@
 def myname("fname, lname"):
     #this functions prints out user names
     #First changes made to this code
+    #added second comment for second commit
     print("My full name is fname lname")
 
     results = myname("Susan", "Dust")
