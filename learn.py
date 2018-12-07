ddbafc5d69bf90b35ac1d7e2c8a61c6e44207394
sylva <ate_sylva@yahoo.com>
2018-12-06 19:00:54 -0700

    added edited files

---
+++ learn.py
@@ -0,0 +1,10 @@
+
+
+def myname("fname, lname"):
+    #this functions prints out user names
+    #First changes made to this code
+    print("My full name is fname lname")
+
+    results = myname("Susan", "Dust")
+
+    return results
