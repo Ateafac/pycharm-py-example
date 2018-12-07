b4a6bb265ea4de2035156e8fbdb19abfdd35065b
sylva <ate_sylva@yahoo.com>
2018-12-07 00:34:36 -0700

    changes made on branch

--- learn.py
+++ learn.py
@@ -4,6 +4,9 @@ def myname("fname, lname"):
     #this functions prints out user names
     #First changes made to this code
     #added second comment for second commit
+
+    #branched pulled from master repo github
+
     print("My full name is fname lname")
 
     results = myname("Susan", "Dust")
