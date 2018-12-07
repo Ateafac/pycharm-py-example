c54f8126b55720c9790e56393f436126bcbddeb8
sylva <ate_sylva@yahoo.com>
2018-12-07 00:51:18 -0700

    conflicts merged

--- learn.py
+++ learn.py
@@ -3,7 +3,9 @@
 def myname("fname, lname"):
     #this functions prints out user names
     #First changes made to this code
-    #added second comment for second commit
+    #added second comment for second commitexit
+
+    #doing second commit for thisfile
     print("My full name is fname lname")
 
     results = myname("Susan", "Dust")
